3e7a12df5e7dbb08f61131c684f7ed9b72c32a09
Nikhil Kohli <nikhilkohli2407@gmail.com>
2025-01-10 13:18:27 +0530

    updateing backend

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://linkedinclone-56pr.onrender.com";
+export const BASE_URL = "https://linkedinclone-4xgi.onrender.com";
 
 export const clientServer = axios.create({
     baseURL: BASE_URL,
