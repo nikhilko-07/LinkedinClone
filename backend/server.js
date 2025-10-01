d27f47c59f2f4ad15bd0295aeb88135877841024
Nikhil Kohli <nikhilkohli2407@gmail.com>
2025-10-01 23:26:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
 import mongoose from "mongoose";
-import postRoutes from './routes/post.routes.js';
-import userRoutes from './routes/user.routes.js';
-dotenv.config();
+import postRoutes from "./routes/post.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 const app = express();
 
 app.use(cors());
@@ -13,13 +12,24 @@ app.use(postRoutes);
 app.use(userRoutes);
 app.use(express.static("uploads"));
 
-
 const start = async () => {
-  const connectDB = await mongoose.connect(
-    "mongodb+srv://nikhil:nikhil@123@cluster0.o5ohf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
-  app.listen(9090,()=>{
-    console.log("server is running on 9090")
-  })
+  try {
+    await mongoose.connect(
+      "mongodb+srv://nikhil:nikhil123@cluster0.o5ohf.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+    console.log("✅ MongoDB connected");
+
+    app.listen(9090, () => {
+      console.log("🚀 Server is running on port 9090");
+    });
+  } catch (error) {
+    console.error("❌ Database connection failed", error);
+    process.exit(1);
+  }
 };
+
 start();
